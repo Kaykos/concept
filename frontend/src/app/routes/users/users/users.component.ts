1b887ac28af6a698d5f13608e9dd7c050f9a8abe
Sergio <fgsergio@outlook.com>
2017-04-17 05:01:23 -0500

    User management added

--- frontend/src/app/routes/users/users/users.component.ts
+++ frontend/src/app/routes/users/users/users.component.ts
@@ -1,10 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
-import {UsersService} from '../../../shared/services/users.service';
-import {AuthService} from '../../../shared/services/auth.service';
+import { UsersService } from '../../../shared/services/users.service';
+import { AuthService } from '../../../shared/services/auth.service';
 
-import {User} from '../../../shared/models/user.model';
+import { User } from '../../../shared/models/user.model';
 
 @Component({
     selector: 'app-users',
@@ -12,30 +12,42 @@ import {User} from '../../../shared/models/user.model';
     styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
-
   private user: User;
-  private error: boolean;
+  private errorPassword: boolean;
+  private errorEmail: boolean;
+  private errorDelete: boolean;
   private errorMessage: string;
   private emailError: boolean;
   private passwordsError: boolean;
   private passwordError: boolean;
+  private passwordChanged: boolean;
+  private emailChanged: boolean;
 
   constructor(private usersService: UsersService, private authService: AuthService, private router: Router) {
-    this.user = new User();
-    this.error = false;
+    this.errorPassword = false;
+    this.errorEmail = false;
+    this.errorDelete = false;
     this.errorMessage = '';
     this.emailError = false;
+    this.passwordsError = false;
     this.passwordError = false;
-    this.passwordError = false;
+    this.passwordChanged = false;
+    this.emailChanged = false;
   }
 
-  ngOnInit() {}
+  /*
+    Get current user
+
+   */
+  ngOnInit() {
+    this.user = this.authService.getCurrentUser();
+  }
 
   /*
     Validates if the second typed password matches first one
 
    */
-  onKeyConfirmPassword(event: KeyboardEvent, password: string, confirmPassword: string) {
+  onKeyConfirmPassword(password: string, confirmPassword: string) {
     this.passwordError = false;
     if (password !== confirmPassword) {
       this.passwordError = true;
@@ -43,11 +55,104 @@ export class UsersComponent implements OnInit {
   }
 
   /*
-   Show error message in page
+    Send request to update user password
 
    */
-  handleError(error: any) {
-    this.error = true;
+  updatePassword(password: string) {
+    this.errorPassword = false;
+    this.passwordsError = false;
+    this.passwordChanged = false;
+    if (password === '') {
+      this.passwordsError = true;
+      return;
+    }
+    this.usersService.update(this.user.id, {'password': password})
+      .subscribe(
+        (user: User)  => { this.managePassword(user) },
+        error => this.handleErrorPassword(error));
+  }
+
+  /*
+    Handle events when password is changed
+
+   */
+  managePassword(user: User) {
+    this.user = user;
+    this.passwordChanged = true;
+  }
+
+  /*
+   Show password error message in page
+
+   */
+  handleErrorPassword(error: any) {
+    this.errorPassword = true;
+    this.errorMessage = error.json().message;
+  }
+
+  /*
+   Send request to update user email
+
+   */
+  updateEmail(email: string) {
+    this.errorEmail = false;
+    this.emailError = false;
+    this.emailChanged = false;
+    if (email === '') {
+      this.emailError = true;
+      return;
+    }
+    this.usersService.update(this.user.id, {'email': email})
+      .subscribe(
+        (user: User)  => { this.manageEmail(user) },
+        error => this.handleErrorEmail(error));
+  }
+
+  /*
+   Handle events when email is changed
+
+   */
+  manageEmail(user: User) {
+    this.user = user;
+    this.emailChanged = true;
+  }
+
+  /*
+   Show email error message in page
+
+   */
+  handleErrorEmail(error: any) {
+    this.errorEmail = true;
+    this.errorMessage = error.json().message;
+  }
+
+  /*
+    Send request to delete user
+
+   */
+  deleteUser() {
+    this.errorDelete = false;
+    this.usersService.delete(this.user.id)
+      .subscribe(
+        success  => { this.manageDelete() },
+        error => this.handleErrorDelete(error));
+  }
+
+  /*
+    Handle events when user is deleted
+
+   */
+  manageDelete() {
+    this.authService.clear();
+    this.router.navigate(['/logIn']);
+  }
+
+  /*
+   Show email error message in page
+
+   */
+  handleErrorDelete(error: any) {
+    this.errorDelete = true;
     this.errorMessage = error.json().message;
   }
 }
--- frontend/src/app/shared/services/users.service.ts
+++ frontend/src/app/shared/services/users.service.ts
@@ -1,7 +1,45 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
+import { Observable } from 'rxjs';
+
+import { environment } from '../../../environments/environment';
+
+import { User } from '../models/user.model';
 
 @Injectable()
 export class UsersService {
   constructor(private http: Http) {}
+
+  /*
+   Put service to update user information
+
+   */
+  update(id: number, body: Object): Observable<User> {
+    const bodyString = JSON.stringify(body);
+  const options = new RequestOptions({
+    headers: new Headers({'Content-Type': 'application/json'})
+  });
+  return this.http.put(`${environment.apiBase}/users/` + id.toString(), bodyString, options)
+    .map((response: Response) => UsersService.fromResponse(response));
+  }
+
+  /*
+   Handle response by mapping data
+
+   */
+  private static fromResponse(response: Response): User {
+    return User.getInstance(response.json());
+  }
+
+  /*
+   Delete service of user
+
+   */
+  delete(id: number) {
+    const options = new RequestOptions({
+      headers: new Headers({'Content-Type': 'application/json'})
+    });
+    return this.http.delete(`${environment.apiBase}/users/` + id.toString(), options)
+      .map((response: Response) => { return response; });
+  }
 }
